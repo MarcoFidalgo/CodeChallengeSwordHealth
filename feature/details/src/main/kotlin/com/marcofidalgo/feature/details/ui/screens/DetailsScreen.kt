318810f4d70d5c29863aa10d9afe34c85f125188
Marco-Fidalgo <m_s_f_@hotmail.com>
2025-03-19 23:43:27 +0000

    scrollable description when it's too big

--- feature/details/src/main/kotlin/com/marcofidalgo/feature/details/ui/screens/DetailsScreen.kt
+++ feature/details/src/main/kotlin/com/marcofidalgo/feature/details/ui/screens/DetailsScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -161,7 +163,9 @@ fun Temperament(temperament: String?) {
 
 @Composable
 fun Description(description: String?) {
-    InfoContainer(stringResource(LR.string.label_description), description)
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        InfoContainer(stringResource(LR.string.label_description), description)
+    }
 }
 
 @Preview(showBackground = true)
